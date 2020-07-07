6b7e7a7888e2fb4c63aaf4af35bf282fa8380ac9
Lia <lia.tabilo@gmail.com>
2020-07-07 17:11:22 +0100

    Add mediaqueries

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium, { StyleRoot } from 'radium';
 import Person from './components/Person/Person';
 
 class Main extends Component {
@@ -55,11 +56,16 @@ class Main extends Component {
 
 	render() {
 		const style = {
-			background: 'white',
+			backgroundColor: 'green',
+			color: 'white',
 			font: 'inherit',
 			border: '1px solid blue',
 			padding: '8px',
 			cursor: 'pointer',
+			':hover': {
+				backgroundColor: 'lightgreen',
+				color: 'white',
+			},
 		};
 
 		let persons = null;
@@ -80,17 +86,35 @@ class Main extends Component {
 					})}
 				</div>
 			);
+
+			style.backgroundColor = 'red';
+			style[':hover'] = {
+				backgroundColor: 'orange',
+				color: 'black',
+			};
 		}
+
+		const classes = [];
+		if (this.state.persons.length <= 2) {
+			classes.push('red');
+		}
+		if (this.state.persons.length <= 1) {
+			classes.push('bold');
+		}
+
 		return (
-			<div className="App">
-				<h1>I'm a React component</h1>
-				<button style={style} onClick={this.togglePersonsHandler}>
-					Toggle Persons
-				</button>
-				{persons}
-			</div>
+			<StyleRoot>
+				<div className="App">
+					<h1>I'm a React component</h1>
+					<p className={classes.join(' ')}>This is really working!</p>
+					<button style={style} onClick={this.togglePersonsHandler}>
+						Toggle Persons
+					</button>
+					{persons}
+				</div>
+			</StyleRoot>
 		);
 	}
 }
 
-export default Main;
+export default Radium(Main);
